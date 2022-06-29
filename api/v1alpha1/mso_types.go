d70944b7191eafb5d78bcb272de0340a449e0287
cm891237 <cmwylie19@gmail.com>
2022-06-29 19:20:49 -0400

    [DEV] Update CRD types

--- api/v1alpha1/mso_types.go
+++ api/v1alpha1/mso_types.go
@@ -17,7 +17,31 @@ limitations under the License.
 package v1alpha1
 
 import (
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// +kubebuilder:validation:Enum=Starburst;Blue
+type ISV string
+
+const (
+	Starburst ISV = "Starburst"
+	Blue      ISV = "Blue"
+)
+
+// Loglevel set log levels of configured components
+// +kubebuilder:validation:Enum=debug;info;warning
+type LogLevel string
+
+const (
+	// Debug Log level
+	Debug LogLevel = "debug"
+
+	// Info Log level
+	Info LogLevel = "info"
+
+	// Warning Log level
+	Warning LogLevel = "warning"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -28,14 +52,39 @@ type MSOSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of MSO. Edit mso_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +optional
+	// +kubebuilder:default="info"
+	LogLevel LogLevel `json:"logLevel,omitempty"`
+
+	// Namespace to deploy mso
+	// optional
+	// +kubebuilder:default="monitoring-stack-operator"
+	Namespace string `json:"namespace,omitempty"`
+
+	// name of ISV to monitor, to determine which servicemonitors to apply
+	// +optional
+	// +kubebuilder:default="Starburst"
+	ISV ISV `json:"isv,omitempty"`
+
+	// Namespace of ISV to monitor
+	IsvNamespace string `json:"isvNamespace,omitempty"`
+
+	// URL to apply manifests
+	// +optional
+	ManifestUrl string `json:"manifestUrl,omitempty"`
+
+	// Define resources requests and limits for Monitoring Stack Pods.
+	// +optional
+	// +kubebuilder:default={requests:{cpu: "100m", memory: "256M"}, limits:{memory: "512M", cpu: "500m"}}
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
 }
 
 // MSOStatus defines the observed state of MSO
 type MSOStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	// MSOs are the names of the mso pods
+	MSOs []string `json:"msos"`
 }
 
 //+kubebuilder:object:root=true
